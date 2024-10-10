8396d3bbb5be0d898d17be2fa154f0cae53ceb6b
Mahmoud Abdelhadi <mahmoudashraf960@yahoo.com>
2024-10-10 00:04:51 -0400

    lint working need to fix ts files

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    parserOptions: {
+      sourceType: "module",
+      tsconfigRootDir: __dirname,
+      project: ["./tsconfig.json"],
+    },
+    env: { node: true },
+  };
+  
