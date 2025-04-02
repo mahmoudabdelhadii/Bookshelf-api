765844b1e0306e7a0bff48e11a74d310c3f175db
mahmoudnft <mahmoud@parallelnft.com>
2025-04-01 23:38:34 -0400

    add library

--- database/migrations/2.do.add-extensions-and-index.sql
+++ database/migrations/2.do.add-extensions-and-index.sql
@@ -1,22 +1,20 @@
-
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
 
-CREATE INDEX books_title_trgm_idx ON server.book USING GIN (title gin_trgm_ops);
+CREATE INDEX books_title_trgm_idx ON server.book
+USING GIN (title gin_trgm_ops);
 
 
-ALTER TABLE server.book
-ADD COLUMN title_tsv tsvector GENERATED ALWAYS AS (
+CREATE INDEX books_title_tsv_idx ON server.book
+USING GIN (
   to_tsvector('english', title) || to_tsvector('arabic', title)
-) STORED;
+);
 
-ALTER TABLE server.book
-ADD COLUMN search_tsv tsvector GENERATED ALWAYS AS (
+
+CREATE INDEX books_search_idx ON server.book
+USING GIN (
   setweight(to_tsvector('english', title), 'A') ||
   setweight(to_tsvector('arabic', title), 'A') ||
-  setweight(to_tsvector('english', description), 'B') ||
-  setweight(to_tsvector('arabic', description), 'B')
-) STORED;
-
-CREATE INDEX books_title_tsv_idx ON server.book USING GIN (title_tsv);
-CREATE INDEX books_search_idx ON server.book USING GIN (search_tsv);
+  setweight(to_tsvector('english', overview), 'B') ||
+  setweight(to_tsvector('arabic', overview), 'B')
+);
